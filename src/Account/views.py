019f402f15e19a545561b77c0136e202ea0ecab2
Steve Njuguna <stevenjuguna93@gmail.com>
2021-08-18 22:19:04 +0300

    Added templates directory

--- src/Account/views.py
+++ src/Account/views.py
@@ -1,3 +1,15 @@
+from django.http import response
 from django.shortcuts import render
 
 # Create your views here.
+def Login(requests):
+    return render(requests, 'Login.html')
+
+def Register(requests):
+    return render(requests, 'Register.html')
+
+def Logout(requests):
+    return render(requests, 'Logout.html')
+
+def Dashboard(requests):
+    return render(requests, 'Dashboard.html')
