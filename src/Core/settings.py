1879d461b9175a95e1240f9f471f1f26a76d7797
Steve Njuguna <stevenjuguna93@gmail.com>
2021-08-22 16:55:28 +0300

    Made changes to Email Setup

--- src/Core/settings.py
+++ src/Core/settings.py
@@ -138,13 +138,13 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email Setup
-EMAIL_FROM_USER = 'stevenjuguna93@gmail.com'
+EMAIL_FROM_USER = 'Your Email Address'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'stevenjuguna93@gmail.com'
-EMAIL_HOST_PASSWORD = 'stevenjugunakamau93'
+EMAIL_HOST_USER = 'Your Email Addres'
+EMAIL_HOST_PASSWORD = 'Your Email Password'
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-secondary',
